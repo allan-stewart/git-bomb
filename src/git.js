12c1deae620932e74f98d8e303f1b70009bb3445
Allan Stewart <allan-stewart@pluralsight.com>
2017-02-02 13:22:53 -0700

    Create class for git operations

---
+++ src/git.js
@@ -0,0 +1,21 @@
+class Git {
+  constructor(executor) {
+    this.executor = executor
+  }
+
+  resetHard(dir, callback) {
+    this.executor(dir, 'git reset --HARD', (err, stdout, stderr) => {
+      let wasSuccessful = !err
+      let message = wasSuccessful ? stdout : getErrorMessage(err, stderr)
+      callback(wasSuccessful, message)
+    })
+  }
+}
+
+const getErrorMessage = (err, stderr) => {
+  if (stderr.length > 0) return stderr
+  if (err.code) return 'Error code: ' + err.code
+  return 'An error occurred'
+}
+
+module.exports = Git
---
+++ test/git.specs.js
@@ -0,0 +1,71 @@
+const Git = require('../src/git')
+const assert = require('assert')
+
+describe('git', () => {
+  let git, executedCommands, results
+  const executor = (directory, command, callback) => {
+    executedCommands.push({dir: directory, command})
+    let result = results.shift()
+    callback(result.err, result.stdout, result.stderr)
+  }
+
+  const setupResult = (err, stdout, stderr) => {
+    results.push({err, stdout, stderr})
+  }
+
+  beforeEach(() => {
+    executedCommands = []
+    results = []
+    git = new Git(executor)
+  })
+
+  describe('resetHard', () => {
+    let dir = '~/fake-dir'
+
+    it('should call git reset --HARD on that directory', done => {
+      setupResult(null, 'HEAD is now at fd66683 empty test file\n', '')
+      git.resetHard(dir, (success, message) => {
+        assert.deepEqual(executedCommands, [
+          {dir, command: 'git reset --HARD'}
+        ])
+        done()
+      })
+    })
+
+    it('should return successful when it works', done => {
+      setupResult(null, 'HEAD is now at fd66683 empty test file\n', '')
+      git.resetHard(dir, (success, message) => {
+        assert.equal(success, true)
+        assert.equal(message, 'HEAD is now at fd66683 empty test file\n')
+        done()
+      })
+    })
+
+    it('should return failure with an error code if stderr is empty', done => {
+      setupResult({code: 'ENOENT'}, '', '')
+      git.resetHard(dir, (success, message) => {
+        assert.equal(success, false)
+        assert.equal(message, 'Error code: ENOENT')
+        done()
+      })
+    })
+
+    it('should return failure with an error message from stderr', done => {
+      setupResult({code: 'ENOENT'}, '', 'Something bad happened')
+      git.resetHard(dir, (success, message) => {
+        assert.equal(success, false)
+        assert.equal(message, 'Something bad happened')
+        done()
+      })
+    })
+
+    it('should return failure with a canned error message if no error code and no stderr', done => {
+      setupResult({}, '', '')
+      git.resetHard(dir, (success, message) => {
+        assert.equal(success, false)
+        assert.equal(message, 'An error occurred')
+        done()
+      })
+    })
+  })
+})
