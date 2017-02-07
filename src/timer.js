79412abf3ede7790598e43dbf9d6dd51a7a9077c
Allan Stewart <allan-stewart@pluralsight.com>
2017-02-07 09:15:31 -0700

    Add a timer class

---
+++ src/timer.js
@@ -0,0 +1,28 @@
+class Timer {
+  constructor(callback) {
+    this.time = 0
+    this.callback = callback
+  }
+
+  start() {
+    if (!this.interval) {
+      this.interval = setInterval(() => {
+        this.time--
+        this.callback(this.time)
+      }, 1000)
+    }
+  }
+
+  pause() {
+    if (this.interval) {
+      clearInterval(this.interval)
+      this.interval = null
+    }
+  }
+
+  reset(value) {
+    this.time = value
+  }
+}
+
+module.exports = Timer
